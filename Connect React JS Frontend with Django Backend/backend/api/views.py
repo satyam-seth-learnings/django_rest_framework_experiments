38844191cd4e1c07a7bbfb20f7095257f731adfb
Satyam Seth <satyam1998.1998@gmail.com>
2024-11-11 00:04:45 +0530

    add StudentListAPIView

--- Connect React JS Frontend with Django Backend/backend/api/views.py
+++ Connect React JS Frontend with Django Backend/backend/api/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework.generics import ListAPIView
+from .models import Student
+from .serializers import StudentSerializer
 
 # Create your views here.
+class StudentListAPIView(ListAPIView):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
